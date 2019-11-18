012156ba4041dd68ea0587167cc0d4150e6f87a7
Robert Nordgren <robert.b.nordgren@gmail.com>
2019-11-18 10:59:17 +0100

    Added Transfer function Added unit tests to verify functionality

---
+++ Bank.WebUI/Models/ViewModels/TransferViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank.WebUI.Models.ViewModels
+{
+    public class TransferViewModel
+    {
+        public int SenderAccountID { get; set; }
+        public int RecipientAccountID { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
